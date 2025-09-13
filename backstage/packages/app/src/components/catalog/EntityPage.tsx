f23bc790b6cb758257bd30709d640ad2691d83cf
0GiS0 <giselatb@outlook.com>
2025-09-13 07:35:07 +0000

    add LightBox component to TechDocsAddons in EntityPage

--- backstage/packages/app/src/components/catalog/EntityPage.tsx
+++ backstage/packages/app/src/components/catalog/EntityPage.tsx
@@ -51,7 +51,7 @@ import {
 } from '@backstage/catalog-model';
 
 import { TechDocsAddons } from '@backstage/plugin-techdocs-react';
-import { ReportIssue } from '@backstage/plugin-techdocs-module-addons-contrib';
+import { ReportIssue, LightBox } from '@backstage/plugin-techdocs-module-addons-contrib';
 
 import {
   EntityKubernetesContent,
@@ -63,6 +63,7 @@ const techdocsContent = (
   <EntityTechdocsContent>
     <TechDocsAddons>
       <ReportIssue />
+      <LightBox />
     </TechDocsAddons>
   </EntityTechdocsContent>
 );
